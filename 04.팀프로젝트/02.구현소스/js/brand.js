72048e3e1c34b9fd61c4bb0bd6b64d7f3ecae847
BbbGoat <oomi9421@gmail.com>
2023-03-01 21:35:00 +0900

    ss

--- 04.팀프로젝트/02.구현소스/js/brand.js
+++ 04.팀프로젝트/02.구현소스/js/brand.js
@@ -12,13 +12,48 @@ window.addEventListener("DOMContentLoaded", () => {
     **********************************/
     // 대상선정
     const tab = document.querySelectorAll(".tab_brand ul > li");
+    const main = document.querySelector("#main");
     // console.log(tab);
     
-    for (let x of tab) {
+    // 순번 변수
+    let idx = 0;
+
+    tab.forEach((x, idx) => {
         x.onclick = () => {
-            console.log(x);
+            // 초기화함수
+            resetToggle(idx);
+            
+            // 1. on 클래스 넣기 / 빼기
+            x.classList.toggle("on");
+            
+            // 2. 이미지 변경
+            const atxt = x.innerText;
+            console.log(atxt);
+
+            // 주소할당변수
+            let chgurl;
+
+            switch (atxt) {
+                case "브랜드 소개" : chgurl = "url(images/brand_intro.jpg)"; break;
+                case "히스토리" : chgurl = "url(images/brand_history.jpg)"; break;
+            } /////// switch case ////////
+
+            main.style.backgroundImage = chgurl;
             
         }; ///// click //////
-    } /////// for of /////////
+    }); /////// for of /////////
+
+    function resetToggle(seq = 1000) {
+        tab.forEach((ele,idx) => {
+            // 호출한 순번과 같으면 넘어가!
+            // if (idx === seq) return;
+            // console.log("li순번: ", idx);
+
+            // 클래스 on 빼기
+            ele.classList.remove("on");
+          }); //////// forEach //////////
+    } ///////////// resetToggle //////////
+
+    
     
 }); //////////////////// 로드구역 //////////////////////
