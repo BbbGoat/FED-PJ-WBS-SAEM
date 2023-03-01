70fdd479c4a3d8e5f0e576992277b60dce24422f
BbbGoat <oomi9421@gmail.com>
2023-03-01 20:13:32 +0900

    ss

--- 04.팀프로젝트/02.구현소스/js/brand.js
+++ 04.팀프로젝트/02.구현소스/js/brand.js
@@ -4,6 +4,21 @@
 // 로드구역 ////////////////////////////////////////
 window.addEventListener("DOMContentLoaded", () => {
     console.log("로딩완료");
+
+    /********************************** 
+        tab_brand 클릭이벤트
+        클래스 "on" 적용
+        각 항목에 맞게 이미지 변경
+    **********************************/
+    // 대상선정
+    const tab = document.querySelectorAll(".tab_brand ul > li");
+    // console.log(tab);
     
+    for (let x of tab) {
+        x.onclick = () => {
+            console.log(x);
+            
+        }; ///// click //////
+    } /////// for of /////////
     
 }); //////////////////// 로드구역 //////////////////////
