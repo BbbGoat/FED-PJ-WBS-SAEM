e36642be2e66997b59943aa25ac68fdeb8c5cf6a
BbbGoat <oomi9421@gmail.com>
2023-03-22 17:14:07 +0900

    Update main.js

--- 01.웹프로젝트/03.구현소스/js/main.js
+++ 01.웹프로젝트/03.구현소스/js/main.js
@@ -6,19 +6,31 @@ window.addEventListener("DOMContentLoaded",()=>{
     const vid1 = document.querySelector("#video1");
     console.log(vid1);
 
+    const vid2 = document.querySelector("#video2");
+
     // 버튼 클릭시 재생/멈춤
     // 버튼대상: .circle_timer
-    const btn_vid = document.querySelector(".circle_timer");
+    const btn_mainvid = document.querySelector(".mainVid .circle_timer");
+    const btn_brandvid = document.querySelector(".brand .circle_timer");
 
 
 
-    btn_vid.addEventListener("click",()=>{
-        console.log("클릭",vid1.paused);
+    btn_mainvid.addEventListener("click",()=>{
+        // console.log("클릭",vid1.paused);
+        event.preventDefault();
 
         if(vid1.paused) vid1.play();
         else vid1.pause();
 
     }); /////////// click //////////////////
+
+    btn_brandvid.addEventListener("click",()=>{
+        event.preventDefault();
+
+        if(vid2.paused) vid2.play();
+        else vid2.pause();
+    }); ////////// click //////////////////
+
 
 
 });///////////// load ////////////////////////////
