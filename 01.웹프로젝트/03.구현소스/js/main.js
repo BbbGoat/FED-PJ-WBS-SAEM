1a06e039f4cd640a6b38511095c11bf2acca2928
BbbGoat <oomi9421@gmail.com>
2023-03-23 23:05:55 +0900

    ss

--- 01.웹프로젝트/03.구현소스/js/main.js
+++ 01.웹프로젝트/03.구현소스/js/main.js
@@ -14,7 +14,7 @@ window.addEventListener("DOMContentLoaded",()=>{
     const btn_brandvid = document.querySelector(".brand .circle_timer");
 
 
-    // 메인비디오 이벤트 설정
+    // 메인비디오 클릭 이벤트 설정
     btn_mainvid.addEventListener("click",()=>{
         // console.log("클릭",vid1.paused);
         event.preventDefault();
@@ -24,7 +24,7 @@ window.addEventListener("DOMContentLoaded",()=>{
 
     }); /////////// click //////////////////
 
-    // 브랜드비디오 이벤트 설정
+    // 브랜드비디오 클릭 이벤트 설정
     btn_brandvid.addEventListener("click",()=>{
         event.preventDefault();
 
@@ -38,34 +38,51 @@ window.addEventListener("DOMContentLoaded",()=>{
     **************************************/
 
     // 동영상전체길이 : svg offset전체길이 = 동영상이동값 : svg offset이동값
-    // 그러므로,
     // svg offset 이동값 = svgoffset전체길이 * 동영상이동값 / 동영상전체길이
     // x = 300% * vidsec_now / vidsec_max
     // x = 300% * vid2.currentTime / vid2.duration
     // 223~300 
 
+    // svg 대상수집
+    const svg1 = document.querySelector(".svg1");
+    const svg2 = document.querySelector(".svg2");
     
-    vid2.addEventListener('timeupdate', () => {
-        
+    // 메인비디오 타임업데이트 이벤트 설정
+    vid1.addEventListener("timeupdate",() => {
+        setInterval(() => {
+            // 영상 재생시간 현재값, 최대값 변수
+            let vidsec_now = vid1.currentTime;
+            let vidsec_max = vid1.duration;
+            // 결과값
+            let timer = (300 * vidsec_now / vidsec_max);
+            // 출력 + 트랜지션
+            svg1.style.strokeDashoffset = (300-timer)+"%";
+            svg1.style.transition = "0.3s ease 0";
+        },100);
+
+        // 인터발초기화
+        clearInterval()
+
+    }); ///////////// timeupdate 이벤트 ////////////
+
+    // 브랜드비디오 타임업데이트 이벤트 설정
+    vid2.addEventListener("timeupdate", () => {
         // 0.5초마다 호출로 값 업데이트
         setInterval(()=>{
             // 영상 재생시간 현재값, 최대값 변수
             let vidsec_now = vid2.currentTime;
             let vidsec_max = vid2.duration;
-            let svg2 = document.querySelector(".svg2");
             // 결과값
-            let timer = ((223-300) * vidsec_now / vidsec_max);
-            // console.log(timer);
-            // 결과값 대입
-            svg2.style.strokeDashoffset = (timer+300)+"%";
-            svg2.style.transition = "0.1s ease 0"
-        },5);
-        
+            let timer = (300 * vidsec_now / vidsec_max);
+            // 출력 + 트랜지션
+            svg2.style.strokeDashoffset = (300-timer)+"%";
+            svg2.style.transition = "0.3s ease 0";
+        },100);
+
+        // 인터발초기화
         clearInterval()
-    });
-    
 
-
+    }); ///////////// timeupdate 이벤트 ////////////
     
     
     // 비디오 끝났을 때 발생하는 이벤트
