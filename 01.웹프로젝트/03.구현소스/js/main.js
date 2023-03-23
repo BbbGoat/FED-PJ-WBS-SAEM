21218c261633b7a98c99650194a9296a9f05683a
BbbGoat <oomi9421@gmail.com>
2023-03-23 17:24:02 +0900

    ss

--- 01.웹프로젝트/03.구현소스/js/main.js
+++ 01.웹프로젝트/03.구현소스/js/main.js
@@ -4,7 +4,7 @@ window.addEventListener("DOMContentLoaded",()=>{
 
     // 비디오대상
     const vid1 = document.querySelector("#video1");
-    console.log(vid1);
+    // console.log(vid1);
 
     const vid2 = document.querySelector("#video2");
 
@@ -32,6 +32,44 @@ window.addEventListener("DOMContentLoaded",()=>{
         else vid2.pause();
     }); ////////// click //////////////////
 
+    
+    /************************************** 
+        영상 길이만큼 svg 움직이기
+    **************************************/
+
+    // 동영상전체길이 : svg offset전체길이 = 동영상이동값 : svg offset이동값
+    // 그러므로,
+    // svg offset 이동값 = svgoffset전체길이 * 동영상이동값 / 동영상전체길이
+    // x = 300% * vidsec_now / vidsec_max
+    // x = 300% * vid2.currentTime / vid2.duration
+    // 223~300 
+
+    
+    vid2.addEventListener('timeupdate', () => {
+        
+        // 0.5초마다 호출로 값 업데이트
+        setInterval(()=>{
+            // 영상 재생시간 현재값, 최대값 변수
+            let vidsec_now = vid2.currentTime;
+            let vidsec_max = vid2.duration;
+            let svg2 = document.querySelector(".svg2");
+            // 결과값
+            let timer = ((223-300) * vidsec_now / vidsec_max);
+            // console.log(timer);
+            // 결과값 대입
+            svg2.style.strokeDashoffset = (timer+300)+"%";
+            svg2.style.transition = "0.1s ease 0"
+        },5);
+        
+        clearInterval()
+    });
+    
+
+
+    
+    
+    // 비디오 끝났을 때 발생하는 이벤트
+    // video.addEventListener('ended', function(e) {});
 
 
 });///////////// load ////////////////////////////
