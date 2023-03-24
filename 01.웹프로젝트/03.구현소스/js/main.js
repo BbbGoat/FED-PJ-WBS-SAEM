a0f78aa442e38e6aab61e9f58624476c2343c1a1
BbbGoat <oomi9421@gmail.com>
2023-03-24 17:18:48 +0900

    ss

--- 01.웹프로젝트/03.구현소스/js/main.js
+++ 01.웹프로젝트/03.구현소스/js/main.js
@@ -1,92 +1,118 @@
 window.addEventListener("DOMContentLoaded",()=>{
 
     console.log("로딩완료!");
-
-    // 비디오대상
-    const vid1 = document.querySelector("#video1");
-    // console.log(vid1);
-
-    const vid2 = document.querySelector("#video2");
-
-    // 버튼 클릭시 재생/멈춤
-    // 버튼대상: .circle_timer
-    const btn_mainvid = document.querySelector(".mainVid .circle_timer");
-    const btn_brandvid = document.querySelector(".brand .circle_timer");
-
-
-    // 메인비디오 클릭 이벤트 설정
-    btn_mainvid.addEventListener("click",()=>{
-        // console.log("클릭",vid1.paused);
-        event.preventDefault();
-
-        if(vid1.paused) vid1.play();
-        else vid1.pause();
-
-    }); /////////// click //////////////////
-
-    // 브랜드비디오 클릭 이벤트 설정
-    btn_brandvid.addEventListener("click",()=>{
-        event.preventDefault();
-
-        if(vid2.paused) vid2.play();
-        else vid2.pause();
-    }); ////////// click //////////////////
-
     
-    /************************************** 
-        영상 길이만큼 svg 움직이기
-    **************************************/
-
-    // 동영상전체길이 : svg offset전체길이 = 동영상이동값 : svg offset이동값
-    // svg offset 이동값 = svgoffset전체길이 * 동영상이동값 / 동영상전체길이
-    // x = 300% * vidsec_now / vidsec_max
-    // x = 300% * vid2.currentTime / vid2.duration
-    // 223~300 
-
-    // svg 대상수집
+    // 전역 변수 선언 ///////////////////
+    const circleTimer = document.querySelector(".circle_timer");
+    const timerBtn = document.querySelectorAll(".circle_timer a");
+    const videoBox = document.querySelector(".video_box");
+    const video = document.querySelectorAll("#video");
     const svg1 = document.querySelector(".svg1");
     const svg2 = document.querySelector(".svg2");
+
+    /******************************************* 
+        함수명: vidClick
+        기능: 클릭이벤트 재생/멈춤
+    *******************************************/
+    function vidClick() {
+        
+        timerBtn.forEach((ele) => {
+            
+            // 클릭버튼 분기
+            ele.onclick = function() {
+                event.preventDefault();
+                
+                // 클릭된 버튼에 따른 변수 설정
+                let currentVideo = this.parentElement.previousElementSibling.querySelector("#video");
+                let timerIcon = this.querySelector(".timer_icon img");
+                
+                // 비디오 멈춤/재생 설정
+                if(currentVideo.paused) {
+                    currentVideo.play();
+                    timerIcon.style.marginTop = "0";
+                    timerIcon.alt = "일시정지";
+                }
+                else {
+                    currentVideo.pause();
+                    timerIcon.style.marginTop = "-25px";
+                    timerIcon.alt = "재생";
+                }
+                
+            }; ////onclick /////////////
+                
+        }); /////////// forEach ////////////
+    } /////////vidClick 함수 //////////////////////////
+
+
+    /******************************************* 
+        함수명: vidTimer
+        기능: 타임업데이트이벤트 svg 설정 변경
+    *******************************************/
+
+    // 동영상전체길이 : dashoffset전체길이 = 동영상이동값 : dashoffset이동값
+    // dashoffset이동값 = svgdashoffset전체길이 * 동영상이동값 / 동영상전체길이
+    // x = 300% * vidsec_now / vidsec_max
+    // x = 300% * vid2.currentTime / vid2.duration
+
+    function vidTimer() {
+
+        video.forEach((ele,idx) => {
+            
+            if (idx === 0) {
+                setInterval(() => {
+                    // 영상 재생시간 현재값, 최대값 변수
+                    let vidsec_now = ele.currentTime;
+                    let vidsec_max = ele.duration;
+
+                    // 결과값
+                    let timer = (300 * vidsec_now / vidsec_max);
+                    // 출력 + 트랜지션
+                    svg1.style.strokeDashoffset = (300-timer)+"%";
+                    svg1.style.transition = "0.3s ease 0";
+                },100);
+        
+                // 인터발초기화
+                clearInterval();
+            }
+            else if (idx === 1) {
+                setInterval(() => {
+                    // 영상 재생시간 현재값, 최대값 변수
+                    let vidsec_now = ele.currentTime;
+                    let vidsec_max = ele.duration;
+                    // 결과값
+                    let timer = (300 * vidsec_now / vidsec_max);
+                    // 출력 + 트랜지션
+                    svg2.style.strokeDashoffset = (300-timer)+"%";
+                    svg2.style.transition = "0.3s ease 0";
+                },100);
+        
+                // 인터발초기화
+                clearInterval();
+            }
+        });
+    } ///////////// vidTimer 함수 ////////////////////
+
     
-    // 메인비디오 타임업데이트 이벤트 설정
-    vid1.addEventListener("timeupdate",() => {
-        setInterval(() => {
-            // 영상 재생시간 현재값, 최대값 변수
-            let vidsec_now = vid1.currentTime;
-            let vidsec_max = vid1.duration;
-            // 결과값
-            let timer = (300 * vidsec_now / vidsec_max);
-            // 출력 + 트랜지션
-            svg1.style.strokeDashoffset = (300-timer)+"%";
-            svg1.style.transition = "0.3s ease 0";
-        },100);
+    /******************************************* 
+        함수명: chgImg
+        기능: 마우스엔터이벤트
+            마우스오버시 타겟박스 내부 이미지 변경
+    *******************************************/
+    function chgImg() {
 
-        // 인터발초기화
-        clearInterval()
 
-    }); ///////////// timeupdate 이벤트 ////////////
+        
+        
 
-    // 브랜드비디오 타임업데이트 이벤트 설정
-    vid2.addEventListener("timeupdate", () => {
-        // 0.5초마다 호출로 값 업데이트
-        setInterval(()=>{
-            // 영상 재생시간 현재값, 최대값 변수
-            let vidsec_now = vid2.currentTime;
-            let vidsec_max = vid2.duration;
-            // 결과값
-            let timer = (300 * vidsec_now / vidsec_max);
-            // 출력 + 트랜지션
-            svg2.style.strokeDashoffset = (300-timer)+"%";
-            svg2.style.transition = "0.3s ease 0";
-        },100);
+    } ///////////// chgImg 함수 /////////////////////
 
-        // 인터발초기화
-        clearInterval()
 
-    }); ///////////// timeupdate 이벤트 ////////////
-    
-    
-    // 비디오 끝났을 때 발생하는 이벤트
-    // video.addEventListener('ended', function(e) {});
+
+
+    // 이벤트 등록 ///////////////////////////////////
+    circleTimer.addEventListener("click", vidClick());
+    videoBox.addEventListener("timeupdate",vidTimer());
+    chgBx.addEventListener("mouseenter", chgImg());
 
 
 });///////////// load ////////////////////////////
