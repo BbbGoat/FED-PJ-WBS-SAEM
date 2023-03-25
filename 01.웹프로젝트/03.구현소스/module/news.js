fcb400e64ca4a37401b63f1b67fcceac835d42f4
BbbGoat <oomi9421@gmail.com>
2023-03-25 23:27:53 +0900

    ss

--- 01.웹프로젝트/03.구현소스/module/news.js
+++ 01.웹프로젝트/03.구현소스/module/news.js
@@ -4,6 +4,7 @@ console.log(newsData);
 
 // 대상수집
 const newsList = document.querySelectorAll(".news_text ul li");
+const newsImg = document.querySelector(".news_img a");
 
 // 0. CJNow 기본틀 셋팅
 // 각 텍스트 배열 순서대로 삽입
@@ -25,14 +26,27 @@ function chgImg() {
         // 마우스엔터 이벤트설정
         ele.onmouseenter = function() {
             event.preventDefault();
-            console.log(this,idx);
+            // console.log(this,idx);
+
             // 이미지 변경
+            // 변경할 주소 변수
+            let src = "";
+            // console.log(newsData);
             switch (idx) {
-                case 0 : imgsrc = "a"; break
-                case 1 : imgsrc = "b"; break
-                case 2 : imgsrc = "c"; break
-                case 3 : imgsrc = "d"; break
+                case 0 : src = `<img src="${newsData.news1.imgData}" alt="${newsData.news1.altData}">`; break
+                case 1 : src = `<img src="${newsData.news2.imgData}" alt="${newsData.news2.altData}">`; break
+                case 2 : src = `<img src="${newsData.news3.imgData}" alt="${newsData.news3.altData}">`; break
+                case 3 : src = `<img src="${newsData.news4.imgData}" alt="${newsData.news4.altData}">`; break
             }
+
+            /////////// for문으로 숫자 변수 증가시키고 그거에 맞춰서
+            // 0부터 3까지 자동으로 셋팅되게 만들기 + news 이름도
+
+            // 마우스엔터시 클래스 on 넣기
+            
+
+            // 출력
+            newsImg.innerHTML = src;
             
         }; //// mouseenter 이벤트 ////
         
--- 01.웹프로젝트/03.구현소스/module/newsData.js
+++ 01.웹프로젝트/03.구현소스/module/newsData.js
@@ -1,21 +1,21 @@
 const newsData = {
     news1 : {
-        imgData : "url(../images/news01.png)",
+        imgData : "./images/news01.png",
         altData : "뉴스이미지1",
         txtData : "CJ대한통운, 택배기사 ‘맞춤형 건강검진’ 확대 …건강한 작업환경 조성 앞장",
     },
     news2 : {
-        imgData : "url(../images/news02.png)",
+        imgData : "./images/news02.png",
         altData : "뉴스이미지2",
         txtData : "“스마트 넘어 인텔리전스 팩토리로” CJ올리브네트웍스, 기계설비 수명도 AI로 관리한다",
     },
     news3 : {
-        imgData : "url(../images/news03.png)",
+        imgData : "./images/news03.png",
         altData : "뉴스이미지3",
         txtData : "CJ올리브영 ‘연매출 100억’ 브랜드 38% 늘었다! 올해 30% 더 늘릴 계획으로 집중 육성",
     },
     news4 : {
-        imgData : "url(../images/news04.png)",
+        imgData : "./images/news04.png",
         altData : "뉴스이미지4",
         txtData : "‘안전한 식자재 유통 앞장’ CJ프레시웨이 식품안전연구실, 경인식약청 표창 수상",
     },
