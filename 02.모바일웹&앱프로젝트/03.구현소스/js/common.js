4c160939a50cf5d6f51ca7cbfd2b2ebdb952bbd2
BbbGoat <oomi9421@gmail.com>
2023-05-07 13:36:43 +0900

    ss

--- 02.모바일웹&앱프로젝트/03.구현소스/js/common.js
+++ 02.모바일웹&앱프로젝트/03.구현소스/js/common.js
@@ -4,7 +4,7 @@ import store from "./store.js";
 /***************************************************** 
     뷰 컴포넌트로 데이터 셋업하기
 *****************************************************/
-// 1. 핸드스킨 컴포넌트
+// [1] 뷰컴포넌트 - 카테고리
 Vue.component("category-comp",{
     template: `
     <ul class="catbx">
@@ -53,6 +53,7 @@ Vue.component("category-comp",{
     }
 }); ///////////////// Vue 컴포넌트 ////////////////////
 
+// [1-2] 뷰컴포넌트 - 카테고리 서브영역
 Vue.component("sub-comp",{
     template: `
     <div class="subbx">
@@ -78,10 +79,83 @@ Vue.component("sub-comp",{
 }); //////////////////// Vue 컴포넌트 ///////////////////////
 
 
+// [2] 뷰컴포넌트 - 푸터
+Vue.component("foot-comp",{
+    template: `
+    <!-- pc용 푸터 -->
+    <section id="dt">
+        <div class="cs_center">
+            <span class="title">고객센터</span>
+            <a href="tel:123-4567" class="cs_number">123:4567</a>
+            <span class="cs_time">월-금 / 10-17시 (점심시간 13-14)</span>
+        </div>
+        <div class="sitemap">
+            <dl class="depth">
+                <dt class="title">주문 및 문의</dt>
+                <dd class="list">
+                    <a href="#">문의하기</a>
+                    <a href="#">자주 묻는 질문</a>
+                    <a href="#">배송</a>
+                    <a href="#">반품</a>
+                    <a href="#">배송 조회하기</a>
+                    <a href="#">주문 내역</a>
+                </dd>
+            </dl>
+            <dl class="depth">
+                <dt class="title">소셜미디어</dt>
+                <dd class="list">
+                    <a href="#">Instagram</a>
+                    <a href="#">Twitter</a>
+                    <a href="#">LinkedIn</a>
+                    <a href="#">Kakao Plus Friend</a>
+                </dd>
+            </dl>
+            <dl class="depth">
+                <dt class="title">소개</dt>
+                <dd class="list">
+                    <a href="#">브랜드 스토리</a>
+                    <a href="#">이솝 재단</a>
+                    <a href="#">채용</a>
+                    <a href="#">개인정보처리방침</a>
+                </dd>
+            </dl>
+            <dl class="depth">
+                <dt class="title">서비스</dt>
+                <dd class="list">
+                    <a href="#">기업체 구매</a>
+                    <a href="#">페이셜 어포인트먼트</a>
+                    <a href="#">1:1 채팅 상담</a>
+                    <a href="#">린스 앤 리턴 캠페인</a>
+                </dd>
+            </dl>
+        </div>
+        <div class="company">
+            <h2>© Aesop</h2>
+            <address class="address">
+                서울특별시 강남구 도산대로45길 10-6 이솝코리아 | 대표자: 프레데리크미셸어완세일러 | 대표전화: 1800-1987 | 대표 이메일: aesop@aesop.com | 
+            </address>
+            <span class="company_number">사업자 등록 번호: 220-88-56014 | </span>
+            <span class="company_infomem">통신판매업 신고번호: 2014-서울강남-02300</span>
+            <p id="copyright">
+                © 2023 Aesop All rights reserved. In God we trust.
+            </p>
+        </div>
+    </section>
+    `,
+}); //////////////////// Vue 컴포넌트 ///////////////////////
 
 
 
 
+
+
+
+
+/********************************************* 
+    뷰인스턴스 생성영역
+*********************************************/
+
+// [1] 뷰인스턴스 - 헤더
 new Vue({
     el: "#top",
     store,
@@ -132,3 +206,12 @@ new Vue({
     } ////////// mounted ///////////
 
 }); ////////////////// Vue 인스턴스 //////////////////////
+
+
+// [2] 뷰인스턴스 - 푸터
+new Vue({
+    el:"#info",
+    store,
+    data: {},
+}); ////////////////// Vue 인스턴스 //////////////////////
+
