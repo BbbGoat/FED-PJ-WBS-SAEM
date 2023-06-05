4a82b37493c69e71aaf4e0a1b7494aa4caf6b034
BbbGoat <oomi9421@gmail.com>
2023-06-05 10:33:07 +0900

    Update Contact.js

--- 03.SPA프로젝트/03.구현소스/studio-app/src/studio/Contact.js
+++ 03.SPA프로젝트/03.구현소스/studio-app/src/studio/Contact.js
@@ -2,12 +2,18 @@
 import React from "react";
 import cat_data from "./data/cat";
 
+// map으로 특정 순번의 키값 출력하는법
+function 예시함수(num){
+    return cat_data.CONTACT.map((x,i)=>x.tit)[num]
+}
+
 const Contact = () => {
 
     return(
         <>
             <h2>Contact 페이지</h2>
-            <h3>{cat_data.CONTACT[1].tit}</h3>
+            {/* <h3>{cat_data.CONTACT[1].tit}</h3> */}
+            <h3>{예시함수(1)}</h3>
             {/* <h3>{cat_data.CONTACT.map((x)=>x.tit)}</h3> */}
         </>
     );
