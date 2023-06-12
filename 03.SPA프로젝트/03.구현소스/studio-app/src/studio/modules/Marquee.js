57390a8513dd35e06613e27629cba3b2ed628937
BbbGoat <oomi9421@gmail.com>
2023-06-12 22:12:51 +0900

    ss

--- 03.SPA프로젝트/03.구현소스/studio-app/src/studio/modules/Marquee.js
+++ 03.SPA프로젝트/03.구현소스/studio-app/src/studio/modules/Marquee.js
@@ -6,27 +6,27 @@ import $ from "jquery";
 const jqFn = () => {
     $(()=>{
 
-        // 대상수집
-        const marquee_item = $(".marquee_item");
-        const marquee_h2 = $(".marquee_item h2");
-        let maxWidth;
+        // // 대상수집
+        // const marquee_item = $(".marquee_item");
+        // const marquee_h2 = $(".marquee_item h2");
+        // let maxWidth;
 
-        function marqueeFn() {
-            maxWidth = window.innerWidth;
-            if (maxWidth > 480) {
-                marquee_item.css({height:"120px"})
-                marquee_h2.css({fontSize:"120px"})
-            }
-            else {
-                marquee_item.css({height:"20vw"})
-                marquee_h2.css({fontSize:"20vw"})
-            }
-        }
+        // function marqueeFn() {
+        //     maxWidth = window.innerWidth;
+        //     if (maxWidth > 480) {
+        //         marquee_item.css({height:"120px"})
+        //         marquee_h2.css({fontSize:"120px"})
+        //     }
+        //     else {
+        //         marquee_item.css({height:"20vw"})
+        //         marquee_h2.css({fontSize:"20vw"})
+        //     }
+        // }
 
-        $(window).on("resize",marqueeFn)
+        // $(window).on("resize",marqueeFn)
 
-        // 로드
-        marqueeFn();
+        // // 로드
+        // marqueeFn();
 
     })
 }
--- 03.SPA프로젝트/03.구현소스/studio-app/src/studio/modules/Welcome.js
+++ 03.SPA프로젝트/03.구현소스/studio-app/src/studio/modules/Welcome.js
@@ -29,9 +29,26 @@ const jqFn = () => {
     $(()=>{
 
         // 대상수집
-        const marquee_item = $(".marquee_item");
-        const marquee_h2 = $(".marquee_item h2");
+        const marquee_item = $(".welcome .marquee_item");
+        const marquee_h2 = $(".welcome .marquee_item h2");
         let maxWidth;
+
+        // 초기값 셋팅
+        marquee_item.css({
+            animationPlayState: "running",
+            opacity:0
+        });
+
+        marquee_item.each((idx,ele)=>{
+            setTimeout(()=>{
+                setTimeout(function(e){
+                    $(ele).animate({
+                        opacity:1
+                    },400)
+                }, 20*idx)
+            },400)
+        })
+        
 
         function marqueeFn() {
             maxWidth = window.innerWidth;
