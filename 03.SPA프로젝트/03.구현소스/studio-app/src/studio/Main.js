8a1e88d35b702194931a569945954108c01bb796
BbbGoat <oomi9421@gmail.com>
2023-06-13 17:03:13 +0900

    ss

--- 03.SPA프로젝트/03.구현소스/studio-app/src/studio/Main.js
+++ 03.SPA프로젝트/03.구현소스/studio-app/src/studio/Main.js
@@ -4,6 +4,7 @@ import React from "react";
 import Surface from "./modules/Surface";
 import Welcome from "./modules/Welcome";
 import Btn from "./modules/Btn";
+import Vidgroup from "./modules/Vidgroup";
 
 const Main = () => {
 
@@ -15,48 +16,8 @@ const Main = () => {
 
             {/* section1 */}
             <Btn cat={"INFO"} />
-            <div className="process-teaser-group">
-                <div className="process-teaser">
-                    {/* Video 영역 */}
-                    <figure className="video">
-                        <video autoPlay={true} className="videosrc" loop={true} muted={true} playsInline={true} poster="" preload="metadata">
-                            <source src="https://www.studiobrot.de/media/pages/wie/0fd2b66c03-1673622947/1-derteig-small.mp4" type="video/mp4" />
-                        </video>
-                    </figure>
-                    {/* text 영역 */}
-                    <div className="process-teaser__overlay">
-                        <span className="process-teaser__number">1</span>
-                        <span className="process-teaser__title">das brot</span>
-                    </div>
-                </div>
-                <div className="process-teaser">
-                    {/* Video 영역 */}
-                    <figure className="video">
-                        <video autoPlay={true} className="videosrc" loop={true} muted={true} playsInline={true} poster="" preload="metadata">
-                            <source src="https://www.studiobrot.de/media/pages/wie/19383b2f5f-1673622957/2-dasbacken-small.mp4" type="video/mp4" />
-                        </video>
-                    </figure>
-                    {/* text 영역 */}
-                    <div className="process-teaser__overlay">
-                        <span className="process-teaser__number">2</span>
-                        <span className="process-teaser__title">das brot</span>
-                    </div>
-                </div>
-                <div className="process-teaser">
-                    {/* Video 영역 */}
-                    <figure className="video">
-                        <video autoPlay={true} className="videosrc" loop={true} muted={true} playsInline={true} poster="" preload="metadata">
-                            <source src="https://www.studiobrot.de/media/pages/wie/b5b0865b86-1673622912/3-dasbrot-small.mp4" type="video/mp4" />
-                        </video>
-                    </figure>
-                    {/* text 영역 */}
-                    <div className="process-teaser__overlay">
-                        <span className="process-teaser__number">3</span>
-                        <span className="process-teaser__title">das brot</span>
-                    </div>
-                </div>
 
-            </div>
+            <Vidgroup />
             
             
             <Btn cat={"WORK"} />
