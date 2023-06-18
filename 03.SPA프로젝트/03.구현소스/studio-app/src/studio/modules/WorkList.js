f8ebe8691bcc18cdc0841596e9bed80607acbd4a
BbbGoat <oomi9421@gmail.com>
2023-06-18 17:53:31 +0900

    ss

---
+++ 03.SPA프로젝트/03.구현소스/studio-app/src/studio/modules/WorkList.js
@@ -0,0 +1,24 @@
+// WorkList 모듈 - WorkList.js
+import $ from "jquery";
+import "../css/worklist.css";
+
+const jqFn = () => {
+    $(()=>{
+
+    }); ///////// jQB ///////////////
+}; ////////////// jqFn /////////////////
+
+
+const WorkList = () => {
+    
+    return (
+        <>
+            <div>
+                
+            </div>
+        </>
+    );
+    
+}; //////////////////// WorkList 컴포넌트 ///////////////////////
+
+export default WorkList;
