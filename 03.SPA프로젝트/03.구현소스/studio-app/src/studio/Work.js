695bdbeffd4af78bb7fb77cf7fb2c142a6cb703d
BbbGoat <oomi9421@gmail.com>
2023-06-20 17:17:45 +0900

    ss

--- 03.SPA프로젝트/03.구현소스/studio-app/src/studio/Work.js
+++ 03.SPA프로젝트/03.구현소스/studio-app/src/studio/Work.js
@@ -1,14 +1,15 @@
 // Work 페이지 컴포넌트
 import React from "react";
 import cat_data from "./data/cat";
+import WorkList from "./modules/WorkList";
+import Welcome from "./modules/Welcome";
 
 const Work = () => {
 
     return(
         <>
-            <h2>Work 페이지</h2>
-            {/* <h3>{cat_data.WORK[0].tit}</h3> */}
-            {/* <h3>{cat_data.WORK.map((x)=>x.tit)}</h3> */}
+            <WorkList />
+            <Welcome />
         </>
     );
 
