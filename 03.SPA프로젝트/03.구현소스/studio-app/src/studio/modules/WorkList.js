7a88ab6005035763b4ab46fdf1ed98aae19ce168
BbbGoat <oomi9421@gmail.com>
2023-06-23 17:09:29 +0900

    ss

--- 03.SPA프로젝트/03.구현소스/studio-app/src/studio/modules/WorkList.js
+++ 03.SPA프로젝트/03.구현소스/studio-app/src/studio/modules/WorkList.js
@@ -16,28 +16,38 @@ const jqFn = () => {
         const svgImg = $(".goo_cont image");
         
         // 스크롤 이벤트 제작
+        // 마우스인!
         $(".project_item").hover(function(e){
             let chgSrc = e.currentTarget.firstChild.getAttribute("src");
-            // console.log(chgSrc);
             
             // 이미지변경!
             svgImg.attr("href", chgSrc);
             goo.css({display:"block"});
-        })
+        });
+        // 마우스아웃!
         $(".project-list").mouseleave(function(){
-            console.log("아웃!");
+            // console.log("아웃!");
             goo.css({display:"none"});
 
+        });
+
+        let winW = window.innerWidth;
+        $(window).on("resize",function(){
+            winW = $(window).width();
+            // console.log(winW);
         })
         
+        
+        // 공식
+        // 윈도우 가로크기 : 박스 크기
         $(document).on("mousemove scroll",function(e){
             // console.log("움직여",e.clientX, e.clientY);
             goo.css({
-                left: `${(e.clientX-100)/2}px`,
+                // left: `calc(${(e.clientX/winW*100)/2}%)`,
+                left: `calc(${(e.clientX/winW*100)/2}% - ${goo.width()/2}px)`,
                 top:`${(e.clientY-300)}px`,
             })
         }) //////// mousemove ////////
-        
     
         
     }); ///////// jQB ///////////////
@@ -62,7 +72,22 @@ const Gooey = () => {
 
                         <mask id="mask" x="0" y="0">
                             <g style={{filter:"url(#gooey)", animation: '9s linear 0s infinite normal none running rotate_back'}}>
-                                <circle cx="50%" cy="60%" r="82" fill="white" style={{animation: '12s ease-in-out -3s infinite alternate none running blob_four'}}>
+                                <circle cx="50%" cy="60%" r="92" fill="white" style={{animation: '12s ease-in-out -3s infinite alternate none running blob_four'}}>
+                                </circle>
+                                <circle cx="50%" cy="40%" r="112" fill="white" style={{animation: '12s ease-in-out -3s infinite alternate none running blob_four'}}>
+                                </circle>
+                                <circle cx="40%" cy="50%" r="80" fill="white" style={{animation: '9s ease-in-out -3s infinite alternate none running blob_three'}}>
+                                </circle>
+                                <circle cx="30%" cy="40%" r="85" fill="white" style={{animation: "6s ease-in-out -3s infinite alternate none running blob_two"}}>
+                                </circle>
+                                <circle cx="40%" cy="70%" r="105" fill="white" style={{animation: "8s ease-in-out -3s infinite alternate none running blob_two"}}>
+                                </circle>
+                                <circle cx="60%" cy="40%" r="94" fill="white" style={{animation: "5s ease-in-out -3s infinite alternate none running blob_one"}}>
+                                </circle>
+                                <circle cx="50%" cy="50%" r="100" fill="white" style={{animation: "3s ease-in-out -3s infinite alternate none running blob_one"}}>
+                                </circle>
+
+                                {/* <circle cx="50%" cy="60%" r="82" fill="white" style={{animation: '12s ease-in-out -3s infinite alternate none running blob_four'}}>
                                 </circle>
                                 <circle cx="50%" cy="50%" r="70" fill="white" style={{animation: '9s ease-in-out -3s infinite alternate none running blob_three'}}>
                                 </circle>
@@ -73,7 +98,7 @@ const Gooey = () => {
                                 <circle cx="50%" cy="50%" r="94" fill="white" style={{animation: "5s ease-in-out -3s infinite alternate none running blob_one"}}>
                                 </circle>
                                 <circle cx="50%" cy="50%" r="60" fill="white" style={{animation: "3s ease-in-out -3s infinite alternate none running blob_one"}}>
-                                </circle>
+                                </circle> */}
                             </g>
                         </mask>
                     </svg>
