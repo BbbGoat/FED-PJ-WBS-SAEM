77613d402dc94147c64084fce1251f984cac1cbe
BbbGoat <oomi9421@gmail.com>
2023-06-24 19:14:48 +0900

    ss

--- 03.SPA프로젝트/03.구현소스/studio-app/src/studio/Cursor.js
+++ 03.SPA프로젝트/03.구현소스/studio-app/src/studio/Cursor.js
@@ -5,7 +5,9 @@ import "./css/cursor.css";
 const jqFn = () => {
   $(() => {
 
+    $("body").css({cursor:"none"})
     const cursor = $(".cursor");
+    const cursor2 = $(".cursor2");
     
     // 걸어야할 이벤트 ///////////////////
     // mousemove mousedown mouseup mouseenter mouseleave
@@ -13,22 +15,27 @@ const jqFn = () => {
     // 변수
     let posX, posY;
     
-    $(window).mousemove(function(e){
-        console.log(e.clientX);
+    $(document).mousemove(function(e){
         // 재할당
         posX = e.clientX;
         posY = e.clientY;
-
+        
         cursor.css({
-            // top : posY,
-            // left: posX,
             transform: `translate3d(calc(${posX}px - 50%), calc(${posY}px - 50%), 0px)`,
-            // transform: translate3d(calc(628px - 50%), calc(282px - 50%), 0px),
-            
         })
+    });
+    $(document).mouseup(function(e){
+        // console.log("떼기!");
+        cursor.removeClass("cursoract");
+        cursor2.removeClass("cursorinteractive");
+    });
+    $(document).mousedown(function(e){
+        // console.log("누르기!");
+        cursor.addClass("cursoract");
+        cursor2.addClass("cursorinteractive");
+    });
 
 
-    })
   });
 };
   
@@ -36,8 +43,8 @@ const Cursor = () => {
   return (
     <>
         <div className="cursor__custom">
-            <div className="cursor cursoract"></div>
-            <div className="cursor2 cursorinteractive"></div>
+            <div className="cursor"></div>
+            <div className="cursor2"></div>
         </div>
         {jqFn()}
     </>
