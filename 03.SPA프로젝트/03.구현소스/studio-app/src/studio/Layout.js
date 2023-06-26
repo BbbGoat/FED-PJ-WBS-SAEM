eccfa0313375ed4e15b18df10608ed79bc3fd4b7
BbbGoat <oomi9421@gmail.com>
2023-06-26 21:06:36 +0900

    Update Layout.js

--- 03.SPA프로젝트/03.구현소스/studio-app/src/studio/Layout.js
+++ 03.SPA프로젝트/03.구현소스/studio-app/src/studio/Layout.js
@@ -22,7 +22,7 @@ const Layout = () => {
     // 랜덤수 출력용
     const [num, setNum] = useState(0);
     function randomNumberInRange(min, max) {return Math.floor(Math.random() * (max - min + 1)) + min;}
-    const handleClick = () => {setNum(randomNumberInRange(0, 2));};
+    const handleClick = () => {setNum(randomNumberInRange(0, 4));};
 
     return (
         <>
