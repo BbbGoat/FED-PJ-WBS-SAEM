b94c50c6b718afb376a0c694df9222ba93db1d08
BbbGoat <oomi9421@gmail.com>
2023-06-27 13:20:11 +0900

    ss

--- 03.SPA프로젝트/03.구현소스/studio-app/src/studio/data/work_data.js
+++ 03.SPA프로젝트/03.구현소스/studio-app/src/studio/data/work_data.js
@@ -3,12 +3,14 @@
 const work_data = [
     {
         "idx":1,
-        "src":"https://www.studiobrot.de/media/pages/arbeiten/charly/8aff3ecd91-1673621027/charly-case-0.webp",
+        "src":"../images/work/04/img-2.webp",
         "tit":"Charly  ",
         "desc":"Corporate Design / Logo / Illustrations / Copywriting / Artwork",
         "detail":"Take your skills to places. Arbeiten und Reisen oder Reisen und Arbeiten oder beim Reisen Arbeiten und beim Arbeiten Reisen? Charly ist die Plattform, die all das möglich macht. Sie bietet Arbeitnehmern und -gebern die Möglichkeit, mit wenigen Klicks die perfekte Destination zu finden. Einen Ort, an dem man alles kann: arbeiten und Urlaub machen. Oder kurz: seinen Lebensmittelpunkt lang- oder kurzfristig verlegen. Alleine oder als Team. Wir fanden das so spannend und dieses Team dazu so charmant, dass wir losgelegt haben mit den Basics. Der Name war klar, das Corporate Design dazu ein Prozess. Gemeinsam mit Team Charly haben wir eine Welt entwickelt, in der all das Platz findet, was Charly ausmacht. Neben der digitalen Denke, auf der hier der Fokus lag, haben wir außerdem Lukas ins Spiel gebracht oder anders: seine Illustrationen. Meet the new normal und checkt Charly, wenn ihr das nächste Mal auf der Suche nach einem Ort seid, der alles kann.&nbsp;",
         "sub":[
             "https://www.studiobrot.de/media/pages/arbeiten/charly/583d305d94-1673621025/charly-case-3-2.webp",
+            "https://www.studiobrot.de/media/pages/arbeiten/charly/098a8732cb-1673621026/charly-case-3.webp",
+            "https://www.studiobrot.de/media/pages/arbeiten/charly/098a8732cb-1673621026/charly-case-3.webp",
             "https://www.studiobrot.de/media/pages/arbeiten/charly/098a8732cb-1673621026/charly-case-3.webp",
         ],
     },
@@ -48,13 +50,17 @@ const work_data = [
 
 const featured_data = {
         "idx":1,
-        "src":"https://www.studiobrot.de/media/pages/arbeiten/myspa/ae7603ad50-1673621037/intro.webp",
-        "tit":"MYSPA  ",
-        "desc":"Campaigning / Art Direction / Corporate Design / Artwork / Copywriting / Illustrations / Social Media",
-        "detail":"studio brot gets ready to rumble, denn auch der härteste Schwergewichts-Boxer braucht einen smoothen Look. Genau den haben wir Victor Daniel verpasst. Neben dem Corporate Design haben wir ein Shooting umgesetzt, aus dem eine Menge schlagfertiger Content für seinen Social Media Kanal entstanden ist. Zudem haben wir uns um die richtigen Punchlines für Merch-Artikel gekümmert. Heavyweight Headlines? Here you go! Hier geben die Texte den Ton an, denn hinter jeder rechten Geraden steckt ein schlauer Kopf. Boxen ist Entertainment und bolde An- und Aussagen gehören genauso dazu, wie eine gute Beinarbeit.",
+        "src":"../images/work/01/img-1.webp",
+        "tit":"chocolate is not the problem  ",
+        "desc":"70% Dark Chocolate / Food / 100g / €12.00",
+        "detail":"A chocolate bar created from the unique David Shrigley work: Chocolate Is Not The Problem (2020) Shrig chocolate has been created in collaboration with Danish chocolate makers Friis Holm, who have been consistently awarded the world's best chocolate at the World Championship of Chocolate Making. Inside David Shrigley's specially designed wrapper is a bespoke chocolate recipe crafted especially for Shrig Shop - three cacao beans from Central America have been perfectly blended - Indio Rojo, Chuno and Nicaliso - to create a smooth and strong chocolate flavour with flowery and red berry notes culminating in a lingering wine acidity. Friis Holm work directly with independent cacao farmers to source the highest quality beans for the best tasting chocolate possible. To ensure quality and fair trade, farmers are paid considerably more for their beans than by standard industry buyers, including “fair trade” branded products, enabling the production of organic, sustainable, delicious chocolate. Buy a single bar, or in multiples of three or six.",
         "sub":[
-            "https://www.studiobrot.de/media/pages/arbeiten/charly/583d305d94-1673621025/charly-case-3-2.webp",
-            "https://www.studiobrot.de/media/pages/arbeiten/charly/098a8732cb-1673621026/charly-case-3.webp",
+            "../images/work/01/img-2.webp",
+            "../images/work/01/img-3.webp",
+            "../images/work/01/img-4.webp",
+            "../images/work/01/img-5.webp",
+            "../images/work/01/img-6.webp",
+            "../images/work/01/img-7.webp",
         ],
     };
 
